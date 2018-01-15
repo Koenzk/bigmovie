ee2aa982aa058337a5e3f938dcc100123596e181
Unknown <rook1600@student.nhl.nl>
2018-01-15 13:37:32 +0100

    Wanneer je vergeett te saven :)

--- SQL/Vragen queries/Vragen Queries.sql
+++ SQL/Vragen queries/Vragen Queries.sql
@@ -1,4 +1,4 @@
---Wat is de kortste film met een waardering van 8.5 of hoger?
+--1. Wat is de kortste film met een waardering van 8.5 of hoger?
 --Getest en werkt
 	SELECT primary_title
 	FROM titles
@@ -9,7 +9,7 @@
 	ORDER BY runtime_minutes ASC
 	LIMIT 1;
 
---Welke regisseur heeft de meeste films met Jim Carrey in de hoofdrol geregisseerd?
+--2. Welke regisseur heeft de meeste films met Jim Carrey in de hoofdrol geregisseerd?
 	SELECT primary_name
 	FROM names
 	WHERE nconst IN (
@@ -28,7 +28,7 @@
 		)
 	);	
 		
---Welke acteur (m/v) heeft de langste filmcarrière? (dus geen series)
+--3. Welke acteur (m/v) heeft de langste filmcarrière? (dus geen series)
 	SELECT primary_name 
 	FROM names
 	WHERE nconst IN (
@@ -37,7 +37,7 @@
 		WHERE 
 	);
 	
---Welke schrijvers spelen in hun eigen films en welke films zijn dat? 
+--4. Welke schrijvers spelen in hun eigen films en welke films zijn dat? 
 	SELECT c.nconst, t.primary_title
 	FROM titles AS t, titles_crew AS c
 	WHERE c.tconst IN (
@@ -46,7 +46,7 @@
 		WHERE c.tconst = p.tconst AND c.role = 'writer'
 	);
 
---Welke acteur of actrice speelt het meest in de slechtst gewaardeerde films?
+--5. Welke acteur of actrice speelt het meest in de slechtst gewaardeerde films?
 	SELECT primary_name 
 	FROM names 
 	WHERE nconst IN (
@@ -59,7 +59,7 @@
 		)
 	);
 
---Welke films van Johnny Depp hebben een 7.5 of hoger?
+--6. Welke films van Johnny Depp hebben een 7.5 of hoger?
 	--Met code
 	SELECT primary_title
 	FROM titles
@@ -91,7 +91,7 @@
 		WHERE average_rating >= 7.5
 	);
 
---In hoeveel Fast And The Furious films speelde Paul Walker?
+--7. In hoeveel Fast And The Furious films speelde Paul Walker?
 	--Met code
 	SELECT COUNT(tconst)
 	FROM titles
@@ -114,7 +114,7 @@
 		)
 	);
 	
---Welke film heeft de hoogste score met de minste stemmen?
+--8. Welke film heeft de hoogste score met de minste stemmen?
 --Getest en werkt
 	SELECT primary_title 
 	FROM titles
@@ -129,3 +129,20 @@
 		ORDER BY num_votes ASC, average_rating DESC
 		LIMIT 1
 	);
+	
+--9. Maak een kaart (b.v. google maps / openstreetview) met landen waar een film speelt. Zodat op de kaart te zien is waar de films spelen. 
+
+	
+--10. Geef het aantal films dat in een land gemaakt is weer in de tijd. Dwz maak een grafiek waarin op de x-as het jaar staat en op de y-as het aantal gemaakte films
+	SELECT akas.tconst, akas.title, akas.region, titles.start_year
+	FROM akas
+    INNER JOIN titles ON akas.title = titles.primary_title
+	WHERE types = '\N' AND title IN (
+		SELECT title
+		FROM akas
+		WHERE is_original_title = 'true' AND tconst = akas.tconst
+	);
+	
+	
+	
+	
