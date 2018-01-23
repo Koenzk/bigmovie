f8882833ecf551febd1e1c9b6342bb009b0bd3aa
Koen <koenzk@hotmail.com>
2018-01-23 13:43:33 +0100

    Chatbot location feature

--- Chatbot/src/com/Chatbot/Chatbot.java
+++ Chatbot/src/com/Chatbot/Chatbot.java
@@ -1,24 +1,14 @@
 package com.Chatbot;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
-import com.google.maps.errors.ApiException;
-import com.google.maps.model.GeocodingResult;
 import com.rivescript.RiveScript;
-import org.telegram.telegrambots.api.methods.send.SendLocation;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-import java.io.IOException;
-
 public class Chatbot extends TelegramLongPollingBot
 {
     private RiveScript bot = new RiveScript();
-    private String apikey = "AIzaSyA4edm9FhbojYzh1lUHcDdJy-ZTnJzQ9tg";
 
     // Constructor
     Chatbot()
@@ -28,85 +18,33 @@ public class Chatbot extends TelegramLongPollingBot
         bot.setSubroutine("bot", new com.Chatbot.BotSubroutine());
         bot.setSubroutine("jdbc", new com.Chatbot.DatabaseSubroutine());
         bot.setSubroutine("send", new com.Chatbot.SendSubroutine(this));
-        bot.loadDirectory("C:\\Users\\5wesl\\Desktop\\bigmovie\\Chatbot\\resources\\rivescript");
-//        bot.loadDirectory("resources/rivescript");
+        bot.loadDirectory("/home/koenzk/chatbot/rivescript");
         bot.sortReplies();
     }
 
     @Override
     @SuppressWarnings("CallToPrintStackTrace")
-    public void onUpdateReceived(Update update) {
-
+    public void onUpdateReceived(Update update)
+    {
         // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
+        if (update.hasMessage() && update.getMessage().hasText())
+        {
             // Set variables
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
 
-            if(message_text.startsWith("give location")){
-                try {
-                    SendLocationMessage(message_text, chat_id);
-                } catch (InterruptedException | IOException | ApiException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                SendRegularMessage(message_text, chat_id);
-            }
-        }
-    }
+            // Get reply
+            String reply = bot.reply(String.valueOf(chat_id), message_text);
 
-    public void Say(String msg, Long cid){
-
-        SendMessage message = new SendMessage() // Create a message object object
-                .setChatId(cid)
-                .setText(msg);
-        if(message != null)
-            try {
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(reply);
+            try
+            {
                 execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
             }
-    }
-
-    public void SendRegularMessage(String usermsg, Long cid){
-        Say(usermsg, cid);
-    }
-
-    /**
-     * send a location message to the user
-     * @param usermsg message sent by the user depends on brain.rive
-     * @param cid chat id
-     */
-    public void SendLocationMessage(String usermsg, Long cid) throws InterruptedException, ApiException, IOException {
-
-        GeoApiContext context = new GeoApiContext.Builder() //creates a geoapicontext needed for using google maps api
-                .apiKey(apikey) //sets the key to token
-                .build(); //builds context
-
-
-        GeocodingResult[] results = GeocodingApi.geocode( //create an array of geocoding results
-                context, //param conext
-                usermsg) //param message
-                .await(); //wait till all results are collected
-
-        if(results.length == 0){ //null check if the message is empty
-            SendRegularMessage("https://en.wikipedia.org/wiki/Nothing", cid);
-            return;
-        }
-
-        SendRegularMessage("I have found " + results.length + "results, here they are", cid);
-
-        for(int i = 0; i < results.length; i++) {
-            SendLocation message = new SendLocation() // Create a locations message object
-                    .setChatId(cid) //set chat id
-                    .setLatitude((float) results[0].geometry.location.lat) //set latitude
-                    .setLongitude((float) results[0].geometry.location.lng); //set longitude
-
-            try {
-                if (message != null)
-                    execute(message); // Sending our message object to user
-
-            } catch (TelegramApiException e) {
+            catch (TelegramApiException e)
+            {
                 e.printStackTrace();
             }
         }
@@ -116,13 +54,13 @@ public class Chatbot extends TelegramLongPollingBot
     public String getBotUsername()
     {
         // Return bot username
-        return "Amsterdammer_bot";
+        return "The_Best_IMDB_bot";
     }
 
     @Override
     public String getBotToken()
     {
         // Return bot token from BotFather
-        return "544145072:AAEyqVbe-x0itYz2tdHdhmDuQTnlhG8N8bI";
+        return "365483835:AAHCHztOCwj8RcU0aDVXQb5RcGdp_aPAlSU";
     }
 }
--- Chatbot/src/com/Chatbot/SendSubroutine.java
+++ Chatbot/src/com/Chatbot/SendSubroutine.java
@@ -1,10 +1,19 @@
 package com.Chatbot;
 
-import com.rivescript.macro.Subroutine;
 import java.io.File;
+import java.io.IOException;
+
+import com.rivescript.macro.Subroutine;
+
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.api.methods.send.SendLocation;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
 
 public class SendSubroutine implements Subroutine
 {
@@ -19,16 +28,16 @@ public class SendSubroutine implements Subroutine
     public String call(com.rivescript.RiveScript rs, String[] args)
     {
         String type = args[0];
-        if ("photo".equals(type))
+        if (type.equals("photo"))
         {
-            String f_id = args[1];
+            String photo = args[1];
             String caption = "";
-            for (int i=2; i<args.length; i++)
+            for (int i = 2; i < args.length; i++)
                 caption += " " + args[i]; // TODO: replace with StringBuilder/StringBuffer (better)
             caption = caption.trim();
             SendPhoto msg = new SendPhoto()
                     .setChatId(rs.currentUser())
-                    .setNewPhoto(new File(f_id))
+                    .setNewPhoto(new File(photo))
                     .setCaption(caption);
             try
             {
@@ -39,6 +48,39 @@ public class SendSubroutine implements Subroutine
                 e.printStackTrace();
             }
         }
+        else if (type.equals("location"))
+        {
+            String location = "";
+            for (int i = 1; i < args.length; i++)
+                location += " " + args[i];
+            location = location.trim();
+
+            try
+            {
+                sender.sendLocation(GetLocationMessage(location, rs.currentUser()));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
         return "";
+    }
+
+    private SendLocation GetLocationMessage(String usermsg, String cid) throws InterruptedException, ApiException, IOException
+    {
+        GeoApiContext context = new GeoApiContext.Builder() //creates a geoapicontext needed for using google maps api
+                .apiKey("AIzaSyA4edm9FhbojYzh1lUHcDdJy-ZTnJzQ9tg") //sets the key to token
+                .build(); //builds context
+
+        GeocodingResult[] results = GeocodingApi.geocode( //create an array of geocoding results
+                context, //param conext
+                usermsg) //param message
+                .await(); //wait till all results are collected
+
+        return new SendLocation()// Create a locations message object
+                .setChatId(cid)
+                .setLatitude((float) results[0].geometry.location.lat)
+                .setLongitude((float) results[0].geometry.location.lng);
     }
 }
