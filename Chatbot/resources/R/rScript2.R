882507af289e9230b7e68b4dd4b2f616052a6586
MustiDarsh <darsh1510@live.nl>
2018-01-23 10:48:00 +0100

    Changed interval years to 10 years throughout the chart

--- Chatbot/resources/R/rScript2.R
+++ Chatbot/resources/R/rScript2.R
@@ -19,7 +19,7 @@ con = dbConnect(pg, user="postgres", password="postgres", host="188.166.120.211"
 
 var1 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 1900 AND 1920 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1900 AND 1910 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
@@ -27,70 +27,77 @@ var1 <- dbGetQuery(con, "SELECT count(original_title)
 
 var2 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 1920 AND 1940 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1910 AND 1920 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var3 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 1940 AND 1960 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1920 AND 1930 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var4 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 1960 AND 1980 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1930 AND 1940 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var5 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 1980 AND 2000 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1940 AND 1950 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var6 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 2000 AND 2003 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1950 AND 1960 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var7 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 2003 AND 2006 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1960 AND 1970 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var8 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 2006 AND 2009 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1970 AND 1980 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var9 <- dbGetQuery(con, "SELECT count(original_title)
                    FROM titles
-                   WHERE start_year BETWEEN 2009 AND 2012 AND title_type = 'movie' AND original_title IN (
+                   WHERE start_year BETWEEN 1980 AND 1990 AND title_type = 'movie' AND original_title IN (
                    SELECT title
                    FROM akas
                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                    )")
 var10 <- dbGetQuery(con, "SELECT count(original_title)
                     FROM titles
-                    WHERE start_year BETWEEN 2012 AND 2015 AND title_type = 'movie' AND original_title IN (
+                    WHERE start_year BETWEEN 1990 AND 2000 AND title_type = 'movie' AND original_title IN (
                     SELECT title
                     FROM akas
                     WHERE types IS NULL AND is_original_title = false AND region = 'NL'
                     )")
 var11 <- dbGetQuery(con, "SELECT count(original_title)
                     FROM titles
-                    WHERE start_year BETWEEN 2015 AND 2018 AND title_type = 'movie' AND original_title IN (
+                    WHERE start_year BETWEEN 2000 AND 2010 AND title_type = 'movie' AND original_title IN (
+                    SELECT title
+                    FROM akas
+                    WHERE types IS NULL AND is_original_title = false AND region = 'NL'
+                    )")
+var12 <- dbGetQuery(con, "SELECT count(original_title)
+                    FROM titles
+                    WHERE start_year BETWEEN 2010 AND 2020 AND title_type = 'movie' AND original_title IN (
                     SELECT title
                     FROM akas
                     WHERE types IS NULL AND is_original_title = false AND region = 'NL'
@@ -99,13 +106,13 @@ var11 <- dbGetQuery(con, "SELECT count(original_title)
 #Create model
 
 #Data for the chart
-H <- c(var1$count, var2$count, var3$count, var4$count, var5$count, var6$count, var7$count, var8$count, var9$count, var10$count, var11$count)
+H <- c(var1$count, var2$count, var3$count, var4$count, var5$count, var6$count, var7$count, var8$count, var9$count, var10$count, var11$count, var12$count)
 
 jpeg(file = "barchart2.jpg", width = 800, height = 600)
 
 barplot(H, col=rainbow(11), main='Aantal films uit nederland', ylab = "Aantal", xlab = "Jaartal, legenda voor meer informatie.", family= "serif", space= 0)
 
-legend("topleft", c("1900-1920","1920-1940","1940-1960","1960-1980","1980-2000", "2000-2003", "2003-2006", "2006-2009", "2009-2012", "2012-2015", "2015-2018"), cex=2, 
+legend("topleft", c("1900-1910","1910-1920","1920-1930","1930-1940","1940-1950", "1950-1960", "1960-1970", "1970-1980", "1980-1990", "1990-2000", "2000-2010", "2010-2020"), cex=2, 
        bty="n", fill=rainbow(11));
 
 dev.off()
